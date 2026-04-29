70b9d66a9e4ad059dbfb209f20d70d447d41c60f
Elsani <elsan.memeti@hotmail.com>
2026-04-29 23:41:26 +0200

    Reducer

---
+++ src/TodosReducer.js
@@ -0,0 +1,10 @@
+export default function todosReducer(todos,action){
+
+    switch(action.type) {
+        case 'deleted': {
+              if (confirm("Are you sure you want to delete the to-do?")) {
+              return todos.filter((todo) => todo.id !== id);
+            } 
+        }
+    }
+}
